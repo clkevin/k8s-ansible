a0257b900bba8d972cf84c1f41999fe1baff6f03
liukai <liu-kevin@foxmail.com>
2018-11-18 17:21:06 +0800

    edit deployment nginx

---
+++ bin/install-k8s.sh
@@ -0,0 +1,8 @@
+
+
+ansible-playbook ~/k8s-ansible/ansible/0.all.yml
+ansible-playbook ~/k8s-ansible/ansible/1.build.yml
+ansible-playbook ~/k8s-ansible/ansible/2.master.yml
+ansible-playbook ~/k8s-ansible/ansible/3.node.yml
+ansible-playbook ~/k8s-ansible/ansible/4.dns.yml
+
